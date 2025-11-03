289adec7a818a25b981f255706694e57952a7992
Lukas Engelter <lukas.engelter@gmail.com>
2025-11-03 08:53:59 +0100

    feature: update papermc download script

--- update-paper-download-link.sh
+++ update-paper-download-link.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 
-# Define the API base URL
-BASE_URL="https://api.papermc.io/v2"
+data=$(curl -s https://qing762.is-a.dev/api/papermc/latest)
 
-# Fetch the list of projects
-project=$(curl -s "$BASE_URL/projects" | jq -r '.projects[0]')
+minecraft_version=$(jq -r '.latest' <<<"$data")
+url=$(jq -r '.url' <<<"$data")
+filename=$(basename "$url")
 
-# Fetch the latest version of the selected project
-version=$(curl -s "$BASE_URL/projects/$project" | jq -r '.versions[-1]')
-
-# Fetch the latest build number for the latest version
-build=$(curl -s "$BASE_URL/projects/$project/versions/$version/builds" | jq -r '.builds[-1].build')
-
-# Get the download file name for the latest build
-file=$(curl -s "$BASE_URL/projects/$project/versions/$version/builds/$build" | jq -r '.downloads.application.name')
-
-# Construct the download URL
-download_url="$BASE_URL/projects/$project/versions/$version/builds/$build/downloads/$file"
-
-# update url in jargroup papermc:
-echo "Latest papermc download URL: $download_url."
-echo "Updating jargroup papermc, please run 'sudo msm jargroup getlatest papermc' to update."
-sudo msm jargroup changeurl papermc "$download_url"
+echo "Latest papermc download version: $filename"
+echo "Updating jargroup papermc…"
+sudo msm jargroup changeurl papermc "$url"
+#sudo msm jargroup getlatest papermc
+echo
+echo "Please run 'sudo msm stable jar papermc' and 'sudo msm stable restart' now to apply the new version to server _stable_."
